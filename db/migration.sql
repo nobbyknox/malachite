cf33acf4efa1ff94ea9641aa13781b38e1a1f5ef
Nobby Knox <nobbyknox@gmail.com>
2015-11-30 18:48:12 +0200

    Update the change log

--- db/migration.sql
+++ db/migration.sql
@@ -1,6 +1,6 @@
--- 2015-11-27
+-- Version 0.9.0
 ALTER TABLE "bookmarks" ADD COLUMN `notes` TEXT;
 
--- 2015-11-30
+-- Version 0.9.1
 ALTER TABLE "bookmarks" ADD COLUMN `thumb` TEXT DEFAULT 'blank.png';
 update bookmarks set thumb = 'blank.png' where thumb is null;
