2b9210db4b61b3adb77ef879f2654cb7bc3c8d1f
Nobby Knox <nobbyknox@gmail.com>
2015-11-30 07:57:26 +0200

    More work on the tile layout of bookmarks

--- db/migration.sql
+++ db/migration.sql
@@ -1,2 +1,6 @@
 -- 2015-11-27
 ALTER TABLE "bookmarks" ADD COLUMN `notes` TEXT;
+
+-- 2015-11-30
+ALTER TABLE "bookmarks" ADD COLUMN `thumb` TEXT DEFAULT 'blank.png';
+update bookmarks set thumb = 'blank.png' where thumb is null;
