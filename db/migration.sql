b0d4dc87e026921ce7eedd3d1e7a23a94b5a8b5d
Nobby Knox <nobbyknox@gmail.com>
2015-12-02 15:25:14 +0200

    System APIs for thumbnail processing

--- db/migration.sql
+++ db/migration.sql
@@ -4,3 +4,11 @@ ALTER TABLE "bookmarks" ADD COLUMN `notes` TEXT;
 -- Version 0.9.1
 ALTER TABLE "bookmarks" ADD COLUMN `thumb` TEXT DEFAULT 'blank.png';
 update bookmarks set thumb = 'blank.png' where thumb is null;
+
+-- Version 0.9.2
+CREATE TABLE "bookmarks_meta" (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`bookmarkId`	INTEGER NOT NULL,
+	`attr`	TEXT NOT NULL,
+	`value`	TEXT NOT NULL
+);
